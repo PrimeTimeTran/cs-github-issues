4d33e4f2dfb8f5edf134db815742c87e7ccf9760
Loi Tran <datloiboi@gmail.com>
2019-06-06 11:44:33 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,58 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    const existingToken = sessionStorage.getItem('token');
+    const accessToken = (window.location.search.split("=")[0] === "?access_token") ? window.location.search.split("=")[1] : null;
+    const clientId = process.env.REACT_APP_CLIENT_ID;
+
+    console.log('REACT_APP_CLIENT_ID', process.env.REACT_APP_CLIENT_ID)
+    console.log('REACT_APP_SECRET_KEY', process.env.REACT_APP_SECRET_KEY)
+
+    if (!accessToken && !existingToken) {
+      window.location.replace(`https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`)
+    }
+
+    if (accessToken) {
+      console.log(`New accessToken: ${accessToken}`);
+
+      sessionStorage.setItem("token", accessToken);
+      this.state = {
+        token: accessToken
+      }
+    }
+
+    if (existingToken) {
+      this.state = {
+        token: existingToken
+      };
+    }
+  }
+  render() {
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+            
+          </p>
+          <h1>SECRET: {process.env.SECRET_KEY}</h1>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
